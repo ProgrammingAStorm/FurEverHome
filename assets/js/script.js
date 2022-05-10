9c92f6321b1de06cea36079f36db2b379d6dc910
cmskorcz <chaseskorey@icloud.com>
2022-05-10 16:17:16 -0400

    images of animal post to page if one is available

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-//im lazy lol
 const ID = '5ojBPITunTAqqOTxJtnD0gylUmUo3CCjakTjPZsivTFN5D9wc4'
 const SECRET = 'j0E7b0TKzxU0qUmPNSGZJ7dsfKpEkgW7PKhr4bvt'
+const $body = $('body');
 
 // Uses ID and SECRET to obtain API access token
 const getToken = () => {
@@ -26,9 +26,35 @@ const getAnimals = (data) => {
         },
         success: function(animals) {
             // Logs array of animals to console
-            console.log(animals)
+            console.log(animals.animals)
+            displayAnimals(animals.animals);
         }
     })
+}
+
+const displayAnimals = (array) => {
+    array.forEach(element => {
+        let $card = $('<div>');
+        let $cardImageDiv = $('<div>');
+        let $cardFigure = $('<figure>');
+        let $image = $('<img>');
+
+        $card.addClass('card');
+        $cardImageDiv.addClass('card-image');
+        $cardFigure.addClass('image is-128x128');
+
+        if (element.primary_photo_cropped === null) {
+            return
+        } else {
+            $image.attr('src', element.primary_photo_cropped.small);
+        }
+
+
+        $cardFigure.append($image);
+        $cardImageDiv.append($cardFigure);
+        $card.append($cardImageDiv);
+        $body.append($card);
+    });
 }
 
 // Currently runs at refresh of page, will eventually be tied to an eventlistener on submit of search
