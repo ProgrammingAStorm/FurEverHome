0f906e0ae3b46fa26e2684f88a443a0b73f4b08c
Mark Pavel <markpavel02@gmail.com>
2022-05-09 21:38:45 -0400

    Key and Secret

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,3 @@
 //im lazy lol
-//key:5ojBPITunTAqqOTxJtnD0gylUmUo3CCjakTjPZsivTFN5D9wc4
-//secret:
+//key: 5ojBPITunTAqqOTxJtnD0gylUmUo3CCjakTjPZsivTFN5D9wc4
+//secret: j0E7b0TKzxU0qUmPNSGZJ7dsfKpEkgW7PKhr4bvt
