2e567677de4515c47ec3aed22b134fa2d4500f5a
cmskorcz <chaseskorey@icloud.com>
2022-05-10 13:37:02 -0400

    added some comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,21 @@
 const ID = '5ojBPITunTAqqOTxJtnD0gylUmUo3CCjakTjPZsivTFN5D9wc4'
 const SECRET = 'j0E7b0TKzxU0qUmPNSGZJ7dsfKpEkgW7PKhr4bvt'
 
+// Uses ID and SECRET to obtain API access token
 const getToken = () => {
     $.ajax({
         type: "POST",
         url: "https://api.petfinder.com/v2/oauth2/token",
         data: `grant_type=client_credentials&client_id=${ID}&client_secret=${SECRET}`,
+        // Upon Success, passes token information to getAnimals function.
         success: function(data) {
             getAnimals(data);
-          //do something when request is successfull
         },
         dataType: "json"
       });
 }
 
+// Uses token to access array of animals, will need to add location query eventually
 const getAnimals = (data) => {
     $.ajax({
         type: 'GET',
@@ -23,9 +25,11 @@ const getAnimals = (data) => {
             Authorization: `Bearer ${data.access_token}`
         },
         success: function(animals) {
+            // Logs array of animals to console
             console.log(animals)
         }
     })
 }
 
+// Currently runs at refresh of page, will eventually be tied to an eventlistener on submit of search
 getToken()
