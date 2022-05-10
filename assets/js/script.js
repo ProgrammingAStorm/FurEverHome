eb7b1ad76731389b71a95391aa84c00535b935a4
cmskorcz <chaseskorey@icloud.com>
2022-05-10 13:30:24 -0400

    token is passed to getAnimals func, which then accesses api endpoint to log animals

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,24 @@ const getToken = () => {
         url: "https://api.petfinder.com/v2/oauth2/token",
         data: `grant_type=client_credentials&client_id=${ID}&client_secret=${SECRET}`,
         success: function(data) {
-          console.log(data);
+            getAnimals(data);
           //do something when request is successfull
         },
         dataType: "json"
       });
+}
+
+const getAnimals = (data) => {
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.petfinder.com/v2/animals',
+        headers: {
+            Authorization: `Bearer ${data.access_token}`
+        },
+        success: function(animals) {
+            console.log(animals)
+        }
+    })
 }
 
 getToken()
