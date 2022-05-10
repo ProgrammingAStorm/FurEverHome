e14054433603f0f0c47a435dc56652a3a00ee9eb
cmskorcz <chaseskorey@icloud.com>
2022-05-10 12:41:50 -0400

    receives api token from post req

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 //im lazy lol
-//key: 5ojBPITunTAqqOTxJtnD0gylUmUo3CCjakTjPZsivTFN5D9wc4
-//secret: j0E7b0TKzxU0qUmPNSGZJ7dsfKpEkgW7PKhr4bvt
+const ID = '5ojBPITunTAqqOTxJtnD0gylUmUo3CCjakTjPZsivTFN5D9wc4'
+const SECRET = 'j0E7b0TKzxU0qUmPNSGZJ7dsfKpEkgW7PKhr4bvt'
+
+const getToken = () => {
+    $.ajax({
+        type: "POST",
+        url: "https://api.petfinder.com/v2/oauth2/token",
+        data: `grant_type=client_credentials&client_id=${ID}&client_secret=${SECRET}`,
+        success: function(data) {
+          console.log(data);
+          //do something when request is successfull
+        },
+        dataType: "json"
+      });
+}
+
+getToken()
