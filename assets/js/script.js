ceb0149b3fd120ef9b398f509c90e938ad3c71b7
cmskorcz <chaseskorey@icloud.com>
2022-05-11 19:26:02 -0400

    reverted back to old way of receiving token

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,36 +11,30 @@ const getToken = () => {
         data: `grant_type=client_credentials&client_id=${ID}&client_secret=${SECRET}`,
         // Upon Success, stores token with a key of 'token'.
         success: function(data) {
-            localStorage.setItem('token', data.access_token);
+            getAnimals(data);
         },
         dataType: "json"
       })
-    //   After completion of request, then rerun getAnimals
-      .then(() => {
-          getAnimals();
-      });
 };
 
 // // Uses token to access array of animals, will need to add location query eventually
-const getAnimals = () => {
-    let TOKEN = localStorage.getItem('token');
+const getAnimals = (data) => {
 
     $.ajax({
         type: 'GET',
         url: 'https://api.petfinder.com/v2/animals',
         headers: {
-            Authorization: `Bearer ${TOKEN}`
+            Authorization: `Bearer ${data.access_token}`
         },
         // Logs animals object if request is successful
         success: function(animals) {
-            console.log(animals)
+            console.log(animals.animals)
         },
-        // If request unsuccessful, run getToken to initiate or renew stored token
+        // If request unsuccessful, log error
         error: function(error) {
-            getToken()
+            console.log(error)
         }
     })
 };
 
-// Currently runs at refresh of page, will eventually be tied to an eventlistener on submit of search
-$tokenButton.on('click', getAnimals);
+$tokenButton.on('click', getToken);
